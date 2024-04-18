d49c7a48e4845b5fa5d65b1a6d39b0ade0b7dd72
Tony K. <tony-koke@outlook.com>
2024-04-18 19:48:17 +0100

    Initial Commit

---
+++ ImageFilters/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ImageFilters
+//
+//  Created by TK  on 18/04/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
