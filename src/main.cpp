35867b2f657bf614729fc6a54054ab9dbf4b2872
Tony K. <tony-koke@outlook.com>
2024-04-19 21:55:11 +0100

    Back to a working state

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,17 @@
 //#include "imageBinaryData.hpp"
 
 int main(int argc, const char * argv[]) {
-    std::cout << "Testing";
+    std::cout << "Testing This\n";
 //    if (argc == 1 || argc > 2) {
 //        std::cout << "Need to input the name of the file when building.\n";
 //        std::cout << "Usage: " << argv[0] << " filename.png  (as an example)\n";
 //        return 0;
 //    }
-//    
+
 //    std::cout << "Starting Program...\n";
-//    
+//
 //    openImage(argv[1]);
-//    
+//
 //    std::cout << "Closing Programm...\n";
     
     return 0;
