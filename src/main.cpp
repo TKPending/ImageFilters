28f60d7c4c2bcf9ff25c00a4b8f3987050a39b8c
Tony K. <tony-koke@outlook.com>
2024-04-19 21:44:16 +0100

    Structure Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
-#include "imageBinaryData.hpp"
+//#include "imageBinaryData.hpp"
 
 int main(int argc, const char * argv[]) {
-    if (argc == 1 || argc > 2) {
-        std::cout << "Need to input the name of the file when building.\n";
-        std::cout << "Usage: " << argv[0] << " filename.png  (as an example)\n";
-        return 0;
-    }
-    
-    std::cout << "Starting Program...\n";
-    
-    openImage(argv[1]);
-    
-    std::cout << "Closing Programm...\n";
+    std::cout << "Testing";
+//    if (argc == 1 || argc > 2) {
+//        std::cout << "Need to input the name of the file when building.\n";
+//        std::cout << "Usage: " << argv[0] << " filename.png  (as an example)\n";
+//        return 0;
+//    }
+//    
+//    std::cout << "Starting Program...\n";
+//    
+//    openImage(argv[1]);
+//    
+//    std::cout << "Closing Programm...\n";
     
     return 0;
 }
