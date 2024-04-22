40accb43948ff4d024764c75aaf3f1313786f50a
Tony K. <tony-koke@outlook.com>
2024-04-22 16:16:20 +0100

    Beginning logic for filtering

--- src/filters/ImageProcessor.cpp
+++ src/filters/ImageProcessor.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
+#include <string>
 
 namespace ImageProcessor {
+
+    void printImageSize(FILE* image) {
+        fseek(image, 0, SEEK_END);
+        long imageSize = ftell(image);
+        
+        std::cout << "/nImage Size: " << imageSize << std::endl;
+    }
+
+    static std::string imageFilter() {
+        std::string chosenFilter;
+    
+        while (true) {
+            std::cout << "/nChoose a filter:\n    1) Black and White\n    2) Grayscale\nYour Answer: ";
+            std::cin >> chosenFilter;
+        
+            if (chosenFilter == "1" || chosenFilter == "2") {
+                return chosenFilter;
+            } else {
+                std::cout << "/nInvalid input. Please enter '1' or '2'.\n";
+            }
+        }
+    }
+
+    bool handleChosenFilter(std::string chosenFilter) {
+        if (chosenFilter == "1") {
+            return true;
+        } else if (chosenFilter == "2") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     bool bmpFileConverter(FILE* pOriginalImage, const char* newImage) {
-        fseek(pOriginalImage, 0, SEEK_END);
-        printf("\n\Ending %ld", ftell(pOriginalImage));
+        printImageSize(pOriginalImage);
         
-        fseek(pOriginalImage, 0, SEEK_SET);
-        printf("\n\nBeginning %ld", ftell(pOriginalImage));
-        
-        return true;
+        std::string chosenFilter = imageFilter();
+        bool filterProcess = handleChosenFilter(chosenFilter);
+
+        return filterProcess;
     };
 }
